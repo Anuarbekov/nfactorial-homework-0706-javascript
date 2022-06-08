7f9f49d4b6f365dfa7aefc2e2ed044356ea23194
Meir <mikonek.a@gmail.com>
2022-06-08 18:21:44 +0600

    commit

--- dj-app/script.js
+++ dj-app/script.js
@@ -1,12 +1,29 @@
 const video = document.getElementById("myVideo");
 const iconBtn = document.getElementById("iconBtn");
-
+const iconMusic = document.getElementById("iconMusic");
+const audio = document.getElementById("music");
 iconBtn.addEventListener("click", () => {
     if(video.paused){
-        iconBtn.innerHTML = '<i class="fa fa-play-circle fa-5x" id="iconBtn"></i>';
+        iconBtn.classList.remove("fa-pause-circle");
+        iconBtn.classList.add("fa-play-circle");
         video.play()
     } else {
-        iconBtn.innerHTML = '<i class="fa fa-pause-circle fa-5x" id="iconBtn"></i>';
+        iconBtn.classList.remove("fa-play-circle");
+        iconBtn.classList.add("fa-pause-circle");
         video.pause()
     }
 });
+
+
+iconMusic.addEventListener("click", () => {
+    if(audio.paused) {
+        iconMusic.removeAttribute("src");
+        iconMusic.setAttribute("src", "play-solid.svg");
+        audio.play();
+    } else {
+
+        iconMusic.removeAttribute("src");
+        iconMusic.setAttribute("src", "pause-solid.svg");
+        audio.pause();
+    }
+})
