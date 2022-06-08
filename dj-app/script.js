162f74445b66814f46c70970a6df159065059e91
Meir <mikonek.a@gmail.com>
2022-06-08 17:48:35 +0600

    com

--- dj-app/script.js
+++ dj-app/script.js
@@ -1,6 +1,7 @@
 const video = document.getElementById("myVideo");
 const iconBtn = document.getElementById("iconBtn");
-iconBtn.addEventListener("click", function(){
+
+iconBtn.addEventListener("click", () => {
     if(video.paused){
         iconBtn.innerHTML = '<i class="fa fa-play-circle fa-5x" id="iconBtn"></i>';
         video.play()
@@ -8,4 +9,4 @@ iconBtn.addEventListener("click", function(){
         iconBtn.innerHTML = '<i class="fa fa-pause-circle fa-5x" id="iconBtn"></i>';
         video.pause()
     }
-})
+});
