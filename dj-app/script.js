4d0ed7d31ac8668298abd25924870a6a817bffe2
Meir <mikonek.a@gmail.com>
2022-06-08 17:32:21 +0600

    com

--- dj-app/script.js
+++ dj-app/script.js
@@ -1,3 +1,11 @@
 const video = document.getElementById("myVideo");
 const iconBtn = document.getElementById("iconBtn");
-console.log(video);
+iconBtn.addEventListener("click", function(){
+    if(video.paused){
+        iconBtn.innerHTML = '<i class="fa fa-play-circle fa-5x" id="iconBtn"></i>';
+        video.play()
+    } else {
+        iconBtn.innerHTML = '<i class="fa fa-pause-circle fa-5x" id="iconBtn"></i>';
+        video.pause()
+    }
+})
